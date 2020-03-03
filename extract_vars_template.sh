1254a74afcef3d9319bacb960d39b5c269b8c5ff
Fernando Giorgetti <fgiorget@redhat.com>
2020-03-03 09:13:00 -0300

    TODO for reading vars dynamically

---
+++ extract_vars_template.sh
@@ -0,0 +1,4 @@
+## TODO Evaluate reading vars dynamically
+##      Point is field name must have some sort of 
+##      identifier to help determining type of value
+egrep -o '\${[^}]+}' subtask.json
